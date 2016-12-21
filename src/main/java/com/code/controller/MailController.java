c7cdd71324db7b0c01dab403746b9710aac26953
yanchhuong <yanchhuo_ng@yahoo.com>
2016-12-21 17:58:10 +0900

    sample

---
+++ src/main/java/com/code/controller/MailController.java
@@ -0,0 +1,25 @@
+package com.code.controller;
+
+import javax.mail.MessagingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.code.mail.SmtpMailSender;
+
+@RestController
+public class MailController {
+	
+	@Autowired
+	private SmtpMailSender smtpMailSender;
+
+	@RequestMapping("/send-mail")
+	public void sendMail() throws MessagingException {
+		
+		smtpMailSender.send("jaelsarah7@gmail.com", "Test mail from Spring", "Howdy");
+		
+	}
+	
+
+}
